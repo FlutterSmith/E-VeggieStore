43167e7deeb32a2f4ce2702bbce80ea335784d2d
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2025-01-10 22:29:28 +0200

    Replace BestSellingGridView with BestSellingGridViewBlocBuilder for improved state management

---
+++ lib/features/home/presentation/views/widgets/best_selling_grid_view_bloc_builder.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_grid_view.dart';
+
+class BestSellingGridViewBlocBuilder extends StatelessWidget {
+  const BestSellingGridViewBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const BestSellingGridView();
+  }
+}
