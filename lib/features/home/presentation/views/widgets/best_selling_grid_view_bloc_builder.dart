d569ac65bb2baf6a820fe1d2158d3b7e411358b1
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2025-01-18 13:01:02 +0200

    Update Flutter SDK to version 3.27.3 and regenerate build artifacts

--- lib/features/home/presentation/views/widgets/best_selling_grid_view_bloc_builder.dart
+++ lib/features/home/presentation/views/widgets/best_selling_grid_view_bloc_builder.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/cubits/products_cubit/products_cubit.dart';
+import 'package:fruits_hub/core/helper/get_dummy_products.dart';
+import 'package:fruits_hub/core/widgets/custom_error_widget.dart';
+import 'package:fruits_hub/core/widgets/fruit_item.dart';
+import 'package:fruits_hub/exports.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_grid_view.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class BestSellingGridViewBlocBuilder extends StatelessWidget {
   const BestSellingGridViewBlocBuilder({
@@ -8,6 +14,23 @@ class BestSellingGridViewBlocBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BestSellingGridView();
+    return BlocBuilder<ProductsCubit, ProductsState>(
+      builder: (context, state) {
+        if (state is ProductsSucess) {
+          return BestSellingGridView(
+            products: state.products,
+          );
+        } else if (state is ProductsFailure) {
+          return SliverToBoxAdapter(
+              child: CustomErrorWidget(error: state.errMessage));
+        } else {
+          return Skeletonizer.sliver(
+              enabled: state is ProductsLoading,
+              child: BestSellingGridView(
+                products: getDummyProductList(),
+              ));
+        }
+      },
+    );
   }
 }
