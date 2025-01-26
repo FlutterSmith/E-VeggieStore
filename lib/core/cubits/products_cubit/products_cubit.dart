8170dcb7144db0377dd6075fb44a10ab3d293923
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2025-01-26 20:35:34 +0200

    Add ProductsCubit and ProductsState for product management

---
+++ lib/core/cubits/products_cubit/products_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_hub/core/entites/product_entity.dart';
+import 'package:fruits_hub/core/repos/products_repo/products_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productsRepo) : super(ProductsInitial());
+  final ProductsRepo productsRepo;
+
+  Future<void> getProducts() async {
+    emit(ProductsLoading());
+    final products = await productsRepo.getProduct();
+    products.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (productsData) => emit(ProductsSucess(productsData)),
+    );
+  }
+}
